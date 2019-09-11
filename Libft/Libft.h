7c766a922e9015d20a5345f66c1eda6f0f3e7a49
Minnie Majikthise <mmajikth@ox-c2.21-school.ru>
2019-09-11 14:54:54 +0300

    Lib

--- Libft/Libft.h
+++ Libft/Libft.h
@@ -6,7 +6,7 @@
 /*   By: mmajikth <mmajikth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 14:28:24 by mmajikth          #+#    #+#             */
-/*   Updated: 2019/09/10 14:28:24 by mmajikth         ###   ########.fr       */
+/*   Updated: 2019/09/11 14:02:09 by mmajikth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@
 # include <stdlib.h>
 # include <string.h>
 
-void *	ft_memalloc(size_t size);
+int		ft_strlen(char *str);
+char	*ft_strdup(char *src);
+void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
-char *	ft_strnew(size_t size);
+char	*ft_strnew(size_t size);
 void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 void	ft_putendl(char const *s);
