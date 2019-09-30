cbecdfcc9188a1af704c27c7f8cd1a5c3218ab1b
Minnie Majikthise <mmajikth@ox-a6.21-school.ru>
2019-09-30 18:30:00 +0300

    new

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmajikth <mmajikth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/30 16:56:06 by mmajikth          #+#    #+#             */
+/*   Updated: 2019/09/30 18:24:38 by mmajikth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 100
+# define MAX_FD 256
+
+# include <unistd.h>
+# include <stdlib.h>
+//# include "libft.h"
+
+#endif
